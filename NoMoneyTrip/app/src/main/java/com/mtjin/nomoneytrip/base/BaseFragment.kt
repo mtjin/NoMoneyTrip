3ce2fb75e8a589f44080a511c213aee322686629
jackjackE <tmddjs210@naver.com>
2020-06-05 15:37:52 +0900

    [base] Add BaseViewModel , BaseFragment

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/base/BaseFragment.kt
@@ -0,0 +1,37 @@
+package com.mtjin.nomoneytrip.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<B : ViewDataBinding>(
+    @LayoutRes val layoutId: Int
+) : Fragment() {
+    lateinit var binding: B
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = this
+        init()
+    }
+
+    abstract fun init()
+
+    protected fun showToast(msg: String) =
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+}
---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/base/BaseViewModel.kt
@@ -0,0 +1,26 @@
+package com.mtjin.nomoneytrip.base
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
+
+abstract class BaseViewModel : ViewModel() {
+    protected val compositeDisposable = CompositeDisposable()
+
+    private val _isLoading = MutableLiveData<Boolean>(false)
+    val isLoading: LiveData<Boolean> get() = _isLoading
+
+    override fun onCleared() {
+        compositeDisposable.dispose()
+        super.onCleared()
+    }
+
+    protected fun showProgress() {
+        _isLoading.value = true
+    }
+
+    protected fun hideProgress() {
+        _isLoading.value = false
+    }
+}
