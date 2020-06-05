f5db66ba77aa0fef99e7d44daca385e86f317c0c
jackjackE <tmddjs210@naver.com>
2020-06-05 17:07:47 +0900

    [view] apply communityFragment xml

--- NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/community/CommunityFragment.kt
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/community/CommunityFragment.kt
@@ -4,7 +4,7 @@ import com.mtjin.nomoneytrip.R
 import com.mtjin.nomoneytrip.base.BaseFragment
 import com.mtjin.nomoneytrip.databinding.FragmentCommunityBinding
 
-class CommunityFragment : BaseFragment<FragmentCommunityBinding>(R.layout.fragment_community){
+class CommunityFragment : BaseFragment<FragmentCommunityBinding>(R.layout.fragment_community) {
     override fun init() {
         TODO("Not yet implemented")
     }
