28024a8f346436d4b3fdeb17ad356e6a422f7f23
jackjackE <tmddjs210@naver.com>
2020-06-06 22:30:44 +0900

    [views] add localPageFragment

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/localpage/LocalPageFragment.kt
@@ -0,0 +1,11 @@
+package com.mtjin.nomoneytrip.views.localpage
+
+import com.mtjin.nomoneytrip.R
+import com.mtjin.nomoneytrip.base.BaseFragment
+import com.mtjin.nomoneytrip.databinding.FragmentLocalPageBinding
+
+class LocalPageFragment : BaseFragment<FragmentLocalPageBinding>(R.layout.fragment_local_page) {
+    override fun init() {
+
+    }
+}
