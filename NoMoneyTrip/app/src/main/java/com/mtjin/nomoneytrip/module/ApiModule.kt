a618b5d6d4971bd27bc9b5456341113044e7f313
jackjackE <tmddjs210@naver.com>
2020-06-11 19:18:04 +0900

    [module] add Koin modules

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/module/ApiModule.kt
@@ -0,0 +1,8 @@
+package com.mtjin.nomoneytrip.module
+
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+val apiModule: Module = module {
+
+}
---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/module/RemoteDataModule.kt
@@ -0,0 +1,7 @@
+package com.mtjin.nomoneytrip.module
+
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+val remoteDataModule: Module = module {
+}
