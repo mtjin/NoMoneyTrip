e8ef847099094e4ff609c523e21fc577684b0b71
jackjackE <tmddjs210@naver.com>
2020-06-11 18:43:50 +0900

    [res, layout] add HomePagerAdapter and itemFragment

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/home/HomePagerAdapter.kt
@@ -0,0 +1,16 @@
+package com.mtjin.nomoneytrip.views.home
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+
+class HomePagerAdapter(fm: FragmentManager, behavior: Int) :
+    FragmentStatePagerAdapter(fm, behavior) {
+
+    private val items: ArrayList<Fragment> = ArrayList()
+    override fun getItem(position: Int): Fragment = items[position]
+
+    override fun getCount(): Int {
+        return items.size
+    }
+}
