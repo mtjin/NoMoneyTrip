9aff2f101f2e1f3455aa4a9eaed10fd8d25d1469
jackjackE <tmddjs210@naver.com>
2020-06-11 19:27:23 +0900

    [views] add SearchFragment

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/search/SearchFragment.kt
@@ -0,0 +1,11 @@
+package com.mtjin.nomoneytrip.views.search
+
+import com.mtjin.nomoneytrip.R
+import com.mtjin.nomoneytrip.base.BaseFragment
+import com.mtjin.nomoneytrip.databinding.FragmentSearchBinding
+
+class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
+    override fun init() {
+        TODO("Not yet implemented")
+    }
+}
