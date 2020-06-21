d89065628c2825a54a26b33f28bff250891466e8
jackjackE <tmddjs210@naver.com>
2020-06-21 16:04:30 +0900

    [res, layout] change file name and design

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/profile/ProfileFragment.kt
@@ -0,0 +1,11 @@
+package com.mtjin.nomoneytrip.views.profile
+
+import com.mtjin.nomoneytrip.R
+import com.mtjin.nomoneytrip.base.BaseFragment
+import com.mtjin.nomoneytrip.databinding.FragmentProfileBinding
+
+class ProfileFragment : BaseFragment<FragmentProfileBinding>(R.layout.fragment_profile) {
+    override fun init() {
+
+    }
+}
