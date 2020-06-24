183f204c2304746b058631262316c08c14af4541
jackjackE <tmddjs210@naver.com>
2020-06-24 13:10:29 +0900

    [views] Add loginActivity

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/email_login/EmailLoginActivity.kt
@@ -0,0 +1,7 @@
+package com.mtjin.nomoneytrip.views.email_login
+
+import com.mtjin.nomoneytrip.R
+import com.mtjin.nomoneytrip.base.BaseActivity
+import com.mtjin.nomoneytrip.databinding.ActivityEmailLoginBinding
+
+class EmailLoginActivity : BaseActivity<ActivityEmailLoginBinding>(R.layout.activity_email_login)
