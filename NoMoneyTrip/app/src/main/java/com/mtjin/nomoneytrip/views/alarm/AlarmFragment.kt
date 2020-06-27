3dd717895e5503c03385f391a32dba853e7e335e
jackjackE <tmddjs210@naver.com>
2020-06-28 01:30:05 +0900

    [views] Add AlarmFragment

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/alarm/AlarmFragment.kt
@@ -0,0 +1,12 @@
+package com.mtjin.nomoneytrip.views.alarm
+
+import com.mtjin.nomoneytrip.R
+import com.mtjin.nomoneytrip.base.BaseFragment
+import com.mtjin.nomoneytrip.databinding.FragmentAlarmBinding
+
+class AlarmFragment : BaseFragment<FragmentAlarmBinding>(R.layout.fragment_alarm) {
+    override fun init() {
+
+    }
+
+}
