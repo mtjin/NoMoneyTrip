1e02b9915153fe5d8fe866ffa4a8d7526de33a47
jackjackE <tmddjs210@naver.com>
2020-06-28 12:00:42 +0900

    [views] Add TourHistoryFragment

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/tour_history/TourHistoryFragment.kt
@@ -0,0 +1,12 @@
+package com.mtjin.nomoneytrip.views.tour_history
+
+import com.mtjin.nomoneytrip.R
+import com.mtjin.nomoneytrip.base.BaseFragment
+import com.mtjin.nomoneytrip.databinding.FragmentTourHistoryBinding
+
+class TourHistoryFragment :
+    BaseFragment<FragmentTourHistoryBinding>(R.layout.fragment_tour_history) {
+    override fun init() {
+
+    }
+}
