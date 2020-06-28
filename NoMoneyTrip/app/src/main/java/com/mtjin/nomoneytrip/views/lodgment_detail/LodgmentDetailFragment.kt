fb9dabf0bd163b28c35fe8c7f3e987e2f13dbfee
jackjackE <tmddjs210@naver.com>
2020-06-28 14:34:19 +0900

    [views] Add LodgmentDetailFragment

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/lodgment_detail/LodgmentDetailFragment.kt
@@ -0,0 +1,12 @@
+package com.mtjin.nomoneytrip.views.lodgment_detail
+
+import com.mtjin.nomoneytrip.R
+import com.mtjin.nomoneytrip.base.BaseFragment
+import com.mtjin.nomoneytrip.databinding.FragmentLodgementDetailBinding
+
+class LodgmentDetailFragment :
+    BaseFragment<FragmentLodgementDetailBinding>(R.layout.fragment_lodgement_detail) {
+    override fun init() {
+        TODO("Not yet implemented")
+    }
+}
