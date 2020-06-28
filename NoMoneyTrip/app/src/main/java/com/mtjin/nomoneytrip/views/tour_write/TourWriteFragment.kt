a24809f2a5511a0a0d976136061d0b7f353e86c3
jackjackE <tmddjs210@naver.com>
2020-06-29 00:28:34 +0900

    [views] Implement TourWriteFragment

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/tour_write/TourWriteFragment.kt
@@ -0,0 +1,12 @@
+package com.mtjin.nomoneytrip.views.tour_write
+
+import com.mtjin.nomoneytrip.R
+import com.mtjin.nomoneytrip.base.BaseFragment
+import com.mtjin.nomoneytrip.databinding.FragmentTourWriteBinding
+
+class TourWriteFragment : BaseFragment<FragmentTourWriteBinding>(R.layout.fragment_tour_write) {
+    override fun init() {
+
+    }
+
+}
