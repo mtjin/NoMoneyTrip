43808514e4224824c7187697cf0bcd98a9639074
jackjackE <tmddjs210@naver.com>
2020-06-29 20:36:24 +0900

    [views] Add ReservationFragment

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/reservation/ReservationFragment.kt
@@ -0,0 +1,12 @@
+package com.mtjin.nomoneytrip.views.reservation
+
+import com.mtjin.nomoneytrip.R
+import com.mtjin.nomoneytrip.base.BaseFragment
+import com.mtjin.nomoneytrip.databinding.FragmentReservationBinding
+
+class ReservationFragment :
+    BaseFragment<FragmentReservationBinding>(R.layout.fragment_reservation) {
+    override fun init() {
+        TODO("Not yet implemented")
+    }
+}
