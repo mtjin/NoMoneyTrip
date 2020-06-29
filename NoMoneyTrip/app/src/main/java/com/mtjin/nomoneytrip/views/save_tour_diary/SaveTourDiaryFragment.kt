d3643d1858819dea48f0231de05c19410e074d11
jackjackE <tmddjs210@naver.com>
2020-06-29 22:12:25 +0900

    [views] Implement SaveTourDiaryFragment

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/save_tour_diary/SaveTourDiaryFragment.kt
@@ -0,0 +1,12 @@
+package com.mtjin.nomoneytrip.views.save_tour_diary
+
+import com.mtjin.nomoneytrip.R
+import com.mtjin.nomoneytrip.base.BaseFragment
+import com.mtjin.nomoneytrip.databinding.FragmentSaveTourDiaryBinding
+
+class SaveTourDiaryFragment :
+    BaseFragment<FragmentSaveTourDiaryBinding>(R.layout.fragment_save_tour_diary) {
+    override fun init() {
+        TODO("Not yet implemented")
+    }
+}
