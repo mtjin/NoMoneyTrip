9813c75fa3f506014bed462854c61de9a8e138b4
jackjackE <tmddjs210@naver.com>
2020-06-29 21:29:33 +0900

    [res, layout] Implement MyTourDiaryFragment

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/my_tour_diary/MyTourDiaryFragment.kt
@@ -0,0 +1,12 @@
+package com.mtjin.nomoneytrip.views.my_tour_diary
+
+import com.mtjin.nomoneytrip.R
+import com.mtjin.nomoneytrip.base.BaseFragment
+import com.mtjin.nomoneytrip.databinding.FragmentMyTourDiaryBinding
+
+class MyTourDiaryFragment :
+    BaseFragment<FragmentMyTourDiaryBinding>(R.layout.fragment_my_tour_diary) {
+    override fun init() {
+        TODO("Not yet implemented")
+    }
+}
