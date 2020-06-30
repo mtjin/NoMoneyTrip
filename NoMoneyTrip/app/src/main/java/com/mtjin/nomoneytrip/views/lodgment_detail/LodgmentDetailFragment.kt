08030ef203c2929ad29904f73f048f257af3e698
jackjackE <tmddjs210@naver.com>
2020-07-01 00:15:04 +0900

    [res, layout] Apply Tmap layout

--- NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/lodgment_detail/LodgmentDetailFragment.kt
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/lodgment_detail/LodgmentDetailFragment.kt
@@ -3,10 +3,16 @@ package com.mtjin.nomoneytrip.views.lodgment_detail
 import com.mtjin.nomoneytrip.R
 import com.mtjin.nomoneytrip.base.BaseFragment
 import com.mtjin.nomoneytrip.databinding.FragmentLodgementDetailBinding
+import com.skt.Tmap.TMapView
+import kotlinx.android.synthetic.main.fragment_lodgement_detail.*
 
 class LodgmentDetailFragment :
     BaseFragment<FragmentLodgementDetailBinding>(R.layout.fragment_lodgement_detail) {
     override fun init() {
-        TODO("Not yet implemented")
+        val tmapview = TMapView(context)
+        tmapview.setSKTMapApiKey(getString(R.string.tmap_key))
+        val tMapView = TMapView(context)
+        tMapView.setSKTMapApiKey(getString(R.string.tmap_key))
+        tmap.addView(tMapView)
     }
 }
