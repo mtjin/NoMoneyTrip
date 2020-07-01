d10153ce200f354c42d5677623a38b6517895342
jackjackE <tmddjs210@naver.com>
2020-07-02 03:07:21 +0900

    [Implement] 달력 세팅

--- NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/reservation_phase_first/ReservationPhaseFirstFragment.kt
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/reservation_phase_first/ReservationPhaseFirstFragment.kt
@@ -1,11 +1,21 @@
 package com.mtjin.nomoneytrip.views.reservation_phase_first
 
+import android.util.Log
+import com.applikeysolutions.cosmocalendar.selection.OnDaySelectedListener
+import com.applikeysolutions.cosmocalendar.selection.RangeSelectionManager
 import com.mtjin.nomoneytrip.R
 import com.mtjin.nomoneytrip.base.BaseFragment
 import com.mtjin.nomoneytrip.databinding.FragmentReservationPhaseFirstBinding
+import kotlinx.android.synthetic.main.fragment_reservation_phase_first.*
 
-class ReservationPhaseFirstFragment :BaseFragment<FragmentReservationPhaseFirstBinding>(R.layout.fragment_reservation_phase_first) {
+class ReservationPhaseFirstFragment :
+    BaseFragment<FragmentReservationPhaseFirstBinding>(R.layout.fragment_reservation_phase_first) {
     override fun init() {
-
+        cv_calendar.selectionManager = RangeSelectionManager(OnDaySelectedListener {
+            Log.e(" CALENDAR ", "========== setSelectionManager ==========")
+            Log.e(" CALENDAR ", "Selected Dates : " + cv_calendar.selectedDates.size)
+            if (cv_calendar.selectedDates.size <= 0) return@OnDaySelectedListener
+            Log.e(" CALENDAR ", "Selected Days : " + cv_calendar.selectedDays)
+        })
     }
 }
