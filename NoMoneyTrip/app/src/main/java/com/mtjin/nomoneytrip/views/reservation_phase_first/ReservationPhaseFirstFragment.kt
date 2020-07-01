66fae9eff43ab641739d9bcb12340f4a67635637
jackjackE <tmddjs210@naver.com>
2020-07-01 19:10:38 +0900

    [res, layout] Add ReservationPhaseFirstFragment

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/reservation_phase_first/ReservationPhaseFirstFragment.kt
@@ -0,0 +1,11 @@
+package com.mtjin.nomoneytrip.views.reservation_phase_first
+
+import com.mtjin.nomoneytrip.R
+import com.mtjin.nomoneytrip.base.BaseFragment
+import com.mtjin.nomoneytrip.databinding.FragmentReservationPhaseFirstBinding
+
+class ReservationPhaseFirstFragment :BaseFragment<FragmentReservationPhaseFirstBinding>(R.layout.fragment_reservation_phase_first) {
+    override fun init() {
+
+    }
+}
