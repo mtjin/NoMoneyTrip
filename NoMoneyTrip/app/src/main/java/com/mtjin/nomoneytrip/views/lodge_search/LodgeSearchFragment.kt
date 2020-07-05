dd2b1b0a3bc5f0381876f2f0678e68cb123001b1
jackjackE <tmddjs210@naver.com>
2020-07-05 23:36:34 +0900

    [Add] 숙박 상품 검색 추가

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/lodge_search/LodgeSearchFragment.kt
@@ -0,0 +1,11 @@
+package com.mtjin.nomoneytrip.views.lodge_search
+
+import com.mtjin.nomoneytrip.R
+import com.mtjin.nomoneytrip.base.BaseFragment
+import com.mtjin.nomoneytrip.databinding.FragmentLodgeSearchBinding
+
+class LodgeSearchFragment : BaseFragment<FragmentLodgeSearchBinding>(R.layout.fragment_lodge_search) {
+    override fun init() {
+        TODO("Not yet implemented")
+    }
+}
