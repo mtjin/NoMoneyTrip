2529daf3288c27abc26214ed9900d60e2775833a
jackjackE <tmddjs210@naver.com>
2020-07-05 21:09:26 +0900

    [Add] 프로필 수정 추가

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/profile_edit/ProfileEditFragment.kt
@@ -0,0 +1,11 @@
+package com.mtjin.nomoneytrip.views.profile_edit
+
+import com.mtjin.nomoneytrip.R
+import com.mtjin.nomoneytrip.base.BaseFragment
+import com.mtjin.nomoneytrip.databinding.FragmentProfileEditBinding
+
+class ProfileEditFragment  : BaseFragment<FragmentProfileEditBinding>(R.layout.fragment_profile_edit) {
+    override fun init() {
+        TODO("Not yet implemented")
+    }
+}
