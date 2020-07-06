11baccac90d8f85e62b5db000186882a30fe1bf4
jackjackE <tmddjs210@naver.com>
2020-07-07 00:12:48 +0900

    [Implement] 숙박예약완료 화면 디자인 구현 및 프래그먼트 추가

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/lodgment_complete/LodgmentCompleteFragment.kt
@@ -0,0 +1,12 @@
+package com.mtjin.nomoneytrip.views.lodgment_complete
+
+import com.mtjin.nomoneytrip.R
+import com.mtjin.nomoneytrip.base.BaseFragment
+import com.mtjin.nomoneytrip.databinding.FragmentLodgmentCompleteBinding
+
+class LodgmentCompleteFragment : BaseFragment<FragmentLodgmentCompleteBinding>(R.layout.fragment_lodgment_complete){
+    override fun init() {
+        TODO("Not yet implemented")
+    }
+
+}
