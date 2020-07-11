f7f7a8e90d5c54595881c986bee99e87fbe21d81
jackjackE <tmddjs210@naver.com>
2020-07-12 00:04:19 +0900

    [Add] 예약내역프래그먼트 추가

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/reservation_history/ReservationHistoryFragment.kt
@@ -0,0 +1,12 @@
+package com.mtjin.nomoneytrip.views.reservation_history
+
+import com.mtjin.nomoneytrip.R
+import com.mtjin.nomoneytrip.base.BaseFragment
+import com.mtjin.nomoneytrip.databinding.FragmentReservationHistoryBinding
+
+class ReservationHistoryFragment :
+    BaseFragment<FragmentReservationHistoryBinding>(R.layout.fragment_reservation_history) {
+    override fun init() {
+
+    }
+}
