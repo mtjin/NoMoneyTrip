7ac9fd34d1d970d81509647449d2f68b826aaf3c
jackjackE <tmddjs210@naver.com>
2020-08-27 17:57:45 +0900

    [Implement] Alarm.kt, AlarmAdapter.kt 추가 및 구현, item_alarm.xml 데이터바인딩 적용

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/data/alarm/Alarm.kt
@@ -0,0 +1,3 @@
+package com.mtjin.nomoneytrip.data.alarm
+
+data class Alarm(val content: String, val timestamp: Long)
---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/alarm/AlarmAdapter.kt
@@ -0,0 +1,56 @@
+package com.mtjin.nomoneytrip.views.alarm
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.mtjin.nomoneytrip.R
+import com.mtjin.nomoneytrip.data.alarm.Alarm
+import com.mtjin.nomoneytrip.databinding.ItemAlarmBinding
+
+class AlarmAdapter :
+    RecyclerView.Adapter<AlarmAdapter.ViewHolder>() {
+    private val items: ArrayList<Alarm> = ArrayList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding: ItemAlarmBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.item_alarm,
+            parent,
+            false
+        )
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        items[position].let {
+            holder.bind(it)
+        }
+    }
+
+    class ViewHolder(private val binding: ItemAlarmBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: Alarm) {
+            binding.item = item
+            binding.executePendingBindings()
+        }
+    }
+
+    fun addItems(items: List<Alarm>) {
+        this.items.addAll(items)
+        notifyDataSetChanged()
+    }
+
+    fun addItem(item: Alarm) {
+        this.items.add(item)
+        notifyDataSetChanged()
+    }
+
+    fun clear() {
+        this.items.clear()
+        notifyDataSetChanged()
+    }
+}
