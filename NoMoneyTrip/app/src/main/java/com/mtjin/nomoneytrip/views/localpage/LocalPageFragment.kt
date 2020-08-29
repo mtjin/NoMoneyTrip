93b552b412196c7581d3615c4b782aba89bc443b
jackjackE <tmddjs210@naver.com>
2020-08-30 01:55:49 +0900

    [Implement] LocalPageFragment.kt 사진 및 지역 이름 세팅 구현

--- NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/localpage/LocalPageFragment.kt
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/localpage/LocalPageFragment.kt
@@ -1,6 +1,7 @@
 package com.mtjin.nomoneytrip.views.localpage
 
 import android.util.Log
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.navArgs
 import com.mtjin.nomoneytrip.R
 import com.mtjin.nomoneytrip.base.BaseFragment
@@ -15,5 +16,163 @@ class LocalPageFragment : BaseFragment<FragmentLocalPageBinding>(R.layout.fragme
 
     private fun processIntent() {
         Log.d(TAG, "LocalPageFragment safeArgs -> " + safeArgs.local)
+        var local = ""
+        when (safeArgs.local) {
+            "seoul" -> {
+                local = "서울"
+                binding.ivLocal.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        thisContext,
+                        R.drawable.seoul
+                    )
+                )
+            }
+            "incheon" -> {
+                local = "인천"
+                binding.ivLocal.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        thisContext,
+                        R.drawable.incheon
+                    )
+                )
+            }
+            "daejeon" -> {
+                local = "대전"
+                binding.ivLocal.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        thisContext,
+                        R.drawable.daejeon
+                    )
+                )
+            }
+            "daegu" -> {
+                local = "대구"
+                binding.ivLocal.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        thisContext,
+                        R.drawable.daegu
+                    )
+                )
+            }
+            "gwangju" -> {
+                local = "광주"
+                binding.ivLocal.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        thisContext,
+                        R.drawable.gwangju
+                    )
+                )
+            }
+            "busan" -> {
+                local = "부산"
+                binding.ivLocal.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        thisContext,
+                        R.drawable.busan
+                    )
+                )
+            }
+            "ulsan" -> {
+                local = "울산"
+                binding.ivLocal.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        thisContext,
+                        R.drawable.ulsan
+                    )
+                )
+            }
+            "sejong" -> {
+                local = "세종"
+                binding.ivLocal.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        thisContext,
+                        R.drawable.sejong
+                    )
+                )
+            }
+            "gyeungi" -> {
+                local = "경기"
+                binding.ivLocal.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        thisContext,
+                        R.drawable.gyeungi
+                    )
+                )
+            }
+            "kangwon" -> {
+                local = "강원"
+                binding.ivLocal.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        thisContext,
+                        R.drawable.kangwon
+                    )
+                )
+            }
+            "choongbuk" -> {
+                local = "충북"
+                binding.ivLocal.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        thisContext,
+                        R.drawable.choongbuk
+                    )
+                )
+            }
+            "choongnam" -> {
+                local = "충남"
+                binding.ivLocal.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        thisContext,
+                        R.drawable.choongnam
+                    )
+                )
+            }
+            "gyungbuk" -> {
+                local = "경북"
+                binding.ivLocal.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        thisContext,
+                        R.drawable.gyungbuk
+                    )
+                )
+            }
+            "gyungnam" -> {
+                local = "경남"
+                binding.ivLocal.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        thisContext,
+                        R.drawable.gyungnam
+                    )
+                )
+            }
+            "jeonbuk" -> {
+                local = "전북"
+                binding.ivLocal.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        thisContext,
+                        R.drawable.jeonbuk
+                    )
+                )
+            }
+            "jeonnam" -> {
+                local = "전남"
+                binding.ivLocal.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        thisContext,
+                        R.drawable.jeonnam
+                    )
+                )
+            }
+            "jeju" -> {
+                local = "제주"
+                binding.ivLocal.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        thisContext,
+                        R.drawable.jeju
+                    )
+                )
+            }
+        }
+        binding.tvLocalTitle.text = local
+
     }
 }
--- NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/search/SearchFragment.kt
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/search/SearchFragment.kt
@@ -21,29 +21,8 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_sea
     private fun initViewModelCallback() {
         with(viewModel) {
             goLocal.observe(this@SearchFragment, Observer {
-                var local = ""
-                when (it) {
-                    "seoul" -> local = "서울"
-                    "incheon" -> local = "인천"
-                    "daejeon" -> local = "대전"
-                    "daegu" -> local = "대구"
-                    "gwangju" -> local = "광주"
-                    "busan" -> local = "부산"
-                    "ulsan" -> local = "울산"
-                    "sejong" -> local = "세종"
-                    "gyeungi" -> local = "경기"
-                    "kangwon" -> local = "강원"
-                    "choongbuk" -> local = "충북"
-                    "choongnam" -> local = "충남"
-                    "gyungbuk" -> local = "경북"
-                    "gyungnam" -> local = "경남"
-                    "jeonbuk" -> local = "전북"
-                    "jeonnam" -> local = "전남"
-                    "jeju" -> local = "제주"
-                }
-                Log.d(TAG, "SearchFragment local -> $local")
                 findNavController().navigate(
-                    SearchFragmentDirections.actionSearchFragmentToLocalpageFragment(local)
+                    SearchFragmentDirections.actionSearchFragmentToLocalpageFragment(it)
                 )
             })
         }
