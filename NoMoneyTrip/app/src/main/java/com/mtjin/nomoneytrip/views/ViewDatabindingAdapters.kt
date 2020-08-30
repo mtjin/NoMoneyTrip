03e1886d3a8c3593627aafe9bf0dd610e0cf6260
jackjackE <tmddjs210@naver.com>
2020-08-30 13:39:09 +0900

    [Implement] ViewDatabindingAdapters.kt setUrlImage() 구현

--- NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/ViewDatabindingAdapters.kt
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/ViewDatabindingAdapters.kt
@@ -1,2 +1,14 @@
 package com.mtjin.nomoneytrip.views
 
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.mtjin.nomoneytrip.R
+
+@BindingAdapter("urlImage")
+fun ImageView.setUrlImage(url: String) {
+    Glide.with(this).load(url)
+        .thumbnail(0.3f)
+        .placeholder(R.drawable.img_product)
+        .into(this)
+}
