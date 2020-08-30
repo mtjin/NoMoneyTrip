693e18619ebb380b06e151ae2e4b7d9fa5ee3b89
jackjackE <tmddjs210@naver.com>
2020-08-30 16:21:15 +0900

    [Revise] ApiClient.kt, ApiInterface.kt 정의

--- NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/api/ApiClient.kt
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/api/ApiClient.kt
@@ -1,5 +1,5 @@
 package com.mtjin.nomoneytrip.api
 
 object ApiClient {
-    const val BASE_URL = "http://api.visitkorea.or.kr/openapi/service/rest/"
+    const val TOUR_BASE_URL = "http://api.visitkorea.or.kr/openapi/service/"
 }
--- NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/api/ApiInterface.kt
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/api/ApiInterface.kt
@@ -1,23 +1,39 @@
 package com.mtjin.nomoneytrip.api
 
 import com.mtjin.nomoneytrip.data.local_page.TourIntroduceResponseBody
+import com.mtjin.nomoneytrip.utils.TOUR_API_KEY
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
+import java.net.URLDecoder
 
 interface ApiInterface {
     /* 관광지 검색
     *  http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList?ServiceKey=GU7pI9Qe5kD1WOmPjAuxSGYzbLGkUJXMnQmy8EyV18c%2FdMkdvBBO1gpgrNoX30AOF%2FyKVxQkdZ45c7hMLOBfzQ%3D%3D&contentTypeId=12&areaCode=1&listYN=Y&MobileOS=ETC&MobileApp=TourAPI3.0_Guide&arrange=A&numOfRows=12&pageNo=1&_type=json
     * */
-    @GET("KorService/areaBasedList")
+    @GET("rest/KorService/areaBasedList")
     fun getTourIntroduce(
-        @Query("ServiceKey") ServiceKey: String = "GU7pI9Qe5kD1WOmPjAuxSGYzbLGkUJXMnQmy8EyV18c%2FdMkdvBBO1gpgrNoX30AOF%2FyKVxQkdZ45c7hMLOBfzQ%3D%3D",
-        @Query("contentTypeId") contentTypeId: Int = 1,
+        @Query("serviceKey") serviceKey: String= URLDecoder.decode(TOUR_API_KEY, "UTF-8"),
         @Query("areaCode") areaCode: Int,
+        @Query("contentTypeId") contentTypeId: Int = 12,
         @Query("listYN") listYN: String = "Y",
         @Query("MobileOS") MobileOS: String = "ETC",
         @Query("MobileApp") MobileApp: String = "TourAPI3.0_Guide",
-        @Query("arrange") arrange: String = "A",
+        @Query("arrange") arrange: String = "P",
+        @Query("numOfRows") numOfRows: Int = 12,
+        @Query("pageNo") pageNo: Int = 1,
+        @Query("_type") _type: String = "json"
+    ): Single<TourIntroduceResponseBody>
+
+    @GET("rest/KorService/areaBasedList?")
+    fun getRestaurantIntroduce(
+        @Query("serviceKey") serviceKey: String = URLDecoder.decode(TOUR_API_KEY, "UTF-8"),
+        @Query("areaCode") areaCode: Int,
+        @Query("contentTypeId") contentTypeId: Int = 39,
+        @Query("listYN") listYN: String = "Y",
+        @Query("MobileOS") MobileOS: String = "AND",
+        @Query("MobileApp") MobileApp: String = "NoMoneyTrip",
+        @Query("arrange") arrange: String = "P",
         @Query("numOfRows") numOfRows: Int = 12,
         @Query("pageNo") pageNo: Int = 1,
         @Query("_type") _type: String = "json"
