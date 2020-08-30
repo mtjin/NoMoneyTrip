53ae12f9b8ea41bccb6ba41b86739acc8c2b9e11
jackjackE <tmddjs210@naver.com>
2020-08-30 15:29:06 +0900

    [Revise] Constants.kt 지역코드 상수 추가

--- NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/utils/Constants.kt
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/utils/Constants.kt
@@ -7,3 +7,22 @@ var FCM_KEY: String =
 
 const val EXTRA_NOTIFICATION_TITLE = "EXTRA_NOTIFICATION_TITLE"
 const val EXTRA_NOTIFICATION_MESSAGE = "EXTRA_NOTIFICATION_MESSAGE"
+
+// area code
+const val SEOUL_CODE = 1
+const val INCHEON_CODE = 2
+const val DAEJEON_CODE = 3
+const val DAEGU_CODE = 4
+const val GWANGJU_CODE = 5
+const val BUSAN_CODE = 6
+const val ULSAN_CODE = 7
+const val SEJONG_CODE = 8
+const val GYEUNGI_CODE = 31
+const val KANGWON_CODE = 32
+const val CHOONGBUK_CODE = 33
+const val CHOONGNAM_CODE = 34
+const val GYUNGBUK_CODE = 35
+const val GYUNGNAM_CODE = 36
+const val JEONBUK_CODE = 37
+const val JEONNAM_CODE = 38
+const val JEJU_CODE = 39
