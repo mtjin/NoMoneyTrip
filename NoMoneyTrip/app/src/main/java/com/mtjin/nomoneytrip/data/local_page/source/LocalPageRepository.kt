67191d2047bcb5abc3ec516362546dbba4a9e7a9
jackjackE <tmddjs210@naver.com>
2020-08-30 14:39:48 +0900

    [Add] LocalPageRepository.kt 추가

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/data/local_page/source/LocalPageRepository.kt
@@ -0,0 +1,6 @@
+package com.mtjin.nomoneytrip.data.local_page.source
+
+interface LocalPageRepository {
+    fun requestTourIntroduces()
+    fun requestRestaurantIntroduces()
+}
---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/data/local_page/source/LocalPageRepositoryImpl.kt
@@ -0,0 +1,11 @@
+package com.mtjin.nomoneytrip.data.local_page.source
+
+class LocalPageRepositoryImpl : LocalPageRepository {
+    override fun requestTourIntroduces() {
+        TODO("Not yet implemented")
+    }
+
+    override fun requestRestaurantIntroduces() {
+        TODO("Not yet implemented")
+    }
+}
