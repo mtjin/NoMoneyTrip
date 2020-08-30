afe73d866d18d7cce65e5b7c32c9a96b2049c0a4
jackjackE <tmddjs210@naver.com>
2020-08-30 14:59:35 +0900

    [Revise] ViewDatabindingAdapters.kt 어댑터 set 함수 추가

--- NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/ViewDatabindingAdapters.kt
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/ViewDatabindingAdapters.kt
@@ -2,8 +2,11 @@ package com.mtjin.nomoneytrip.views
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.mtjin.nomoneytrip.R
+import com.mtjin.nomoneytrip.data.local_page.TourIntroduce
+import com.mtjin.nomoneytrip.views.localpage.LocalPageAdapter
 
 @BindingAdapter("urlImage")
 fun ImageView.setUrlImage(url: String) {
@@ -11,4 +14,18 @@ fun ImageView.setUrlImage(url: String) {
         .thumbnail(0.3f)
         .placeholder(R.drawable.img_product)
         .into(this)
+}
+
+@BindingAdapter("setItems")
+fun RecyclerView.setAdapterItems(items: List<Any>?) {
+    when (adapter) {
+        is LocalPageAdapter -> {
+            items?.let {
+                with(adapter as LocalPageAdapter) {
+                    clear()
+                    addItems(it as List<TourIntroduce>)
+                }
+            }
+        }
+    }
 }
