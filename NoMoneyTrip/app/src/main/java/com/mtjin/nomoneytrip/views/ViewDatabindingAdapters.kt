49affc20d30e4b202ed3129a9a1b64f3d5bfdfce
jackjackE <tmddjs210@naver.com>
2020-08-30 16:19:43 +0900

    [Revise] ViewDatabindingAdapters.kt 어댑터 error 추가

--- NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/ViewDatabindingAdapters.kt
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/ViewDatabindingAdapters.kt
@@ -4,13 +4,15 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.mtjin.nomoneytrip.R
 import com.mtjin.nomoneytrip.data.local_page.TourIntroduce
 import com.mtjin.nomoneytrip.views.localpage.LocalPageAdapter
 
 @BindingAdapter("urlImage")
 fun ImageView.setUrlImage(url: String) {
     Glide.with(this).load(url)
-        .thumbnail(0.3f)
+        .thumbnail(0.1f)
+        .error(R.drawable.img_product)
         .into(this)
 }
 
