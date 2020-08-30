c8698acb831fbf4c0bf974fbc9e5bfd724ae3ffc
jackjackE <tmddjs210@naver.com>
2020-08-30 16:20:48 +0900

    [Implement] LocalPageRepository.kt 구현

--- NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/data/local_page/source/LocalPageRepository.kt
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/data/local_page/source/LocalPageRepository.kt
@@ -1,6 +1,10 @@
 package com.mtjin.nomoneytrip.data.local_page.source
 
+import com.mtjin.nomoneytrip.data.local_page.TourIntroduce
+import com.mtjin.nomoneytrip.data.local_page.TourIntroduceResponseBody
+import io.reactivex.Single
+
 interface LocalPageRepository {
-    fun requestTourIntroduces()
-    fun requestRestaurantIntroduces()
+    fun requestTourIntroduces(areaCode: Int): Single<List<TourIntroduce>>
+    fun requestRestaurantIntroduces(areaCode: Int): Single<List<TourIntroduce>>
 }
--- NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/data/local_page/source/LocalPageRepositoryImpl.kt
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/data/local_page/source/LocalPageRepositoryImpl.kt
@@ -1,11 +1,28 @@
 package com.mtjin.nomoneytrip.data.local_page.source
 
-class LocalPageRepositoryImpl : LocalPageRepository {
-    override fun requestTourIntroduces() {
-        TODO("Not yet implemented")
-    }
+import android.util.Log
+import com.mtjin.nomoneytrip.api.ApiInterface
+import com.mtjin.nomoneytrip.data.local_page.TourIntroduce
+import com.mtjin.nomoneytrip.utils.TAG
+import io.reactivex.Single
 
-    override fun requestRestaurantIntroduces() {
-        TODO("Not yet implemented")
-    }
+class LocalPageRepositoryImpl(private val apiInterface: ApiInterface) : LocalPageRepository {
+    override fun requestTourIntroduces(areaCode: Int): Single<List<TourIntroduce>> =
+        apiInterface.getTourIntroduce(areaCode = areaCode).flatMap {
+            Log.d(
+                TAG,
+                "LocalPageRepositoryImpl requestTourIntroduces() -> " + it.tourIntroduceResponse.tourIntroduceHeader.resultMsg
+            )
+            Single.just(it.tourIntroduceResponse.tourIntroduceBody.tourIntroduceList.tourIntroduce)
+        }
+
+
+    override fun requestRestaurantIntroduces(areaCode: Int): Single<List<TourIntroduce>> =
+        apiInterface.getRestaurantIntroduce(areaCode = areaCode).flatMap {
+            Log.d(
+                TAG,
+                "LocalPageRepositoryImpl requestRestaurantIntroduces() -> " + it.tourIntroduceResponse.tourIntroduceHeader.resultMsg
+            )
+            Single.just(it.tourIntroduceResponse.tourIntroduceBody.tourIntroduceList.tourIntroduce)
+        }
 }
