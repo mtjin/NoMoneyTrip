cd06c56e2cb3a98aacd0f18cf12a95546a3e7b43
jackjackE <tmddjs210@naver.com>
2020-08-31 20:46:52 +0900

    [Revise] Product.kt data class 구조 변경

--- NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/data/home/Product.kt
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/data/home/Product.kt
@@ -4,6 +4,7 @@ data class Product(
     var id: String = "",
     var image: String = "",
     var content: String = "",
+    var city: String = "",
     var local: String = "",
     var hashTagList: List<String> = ArrayList()
 )
