0defe8ca6229c08a8ea885a88fc3712ee8277dcc
jackjackE <tmddjs210@naver.com>
2020-09-02 23:00:31 +0900

    [Add] UserReview.kt 추가

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/data/community/UserReview.kt
@@ -0,0 +1,15 @@
+package com.mtjin.nomoneytrip.data.community
+
+//TODO:: 리뷰 받아올때 리뷰아이템과 유저정보 조합해서 UserReview 클래스로 받아올 예정
+class UserReview(
+    var id: String = "",
+    var userId: String = "",
+    var timestamp: Long = 0,
+    var image: String = "",
+    var title: String = "",
+    var content: String = "",
+    var recommend: Long = 0,
+    var local: String = "",
+    var name: String = "",
+    var userImage: String = ""
+)
