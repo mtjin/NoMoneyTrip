9a3d5d2f459f8d72d40061b6fc3f2d73f596485e
jackjackE <tmddjs210@naver.com>
2020-09-05 23:08:39 +0900

    [Implement] TourNoHistoryFragment.kt, fragment_tour_no_history.xml 추가 및 디자인 구현

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/tour_no_history/TourNoHistoryFragment.kt
@@ -0,0 +1,12 @@
+package com.mtjin.nomoneytrip.views.tour_no_history
+
+import com.mtjin.nomoneytrip.R
+import com.mtjin.nomoneytrip.base.BaseFragment
+import com.mtjin.nomoneytrip.databinding.FragmentTourNoHistoryBinding
+
+class TourNoHistoryFragment :
+    BaseFragment<FragmentTourNoHistoryBinding>(R.layout.fragment_tour_no_history) {
+    override fun init() {
+
+    }
+}
