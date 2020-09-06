43b7054d2287d93e02ae7664322aa23be3516f2b
jackjackE <tmddjs210@naver.com>
2020-09-06 20:08:43 +0900

    [Revise] Product.kt 이미지 리스트로 수정

--- NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/data/home/Product.kt
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/data/home/Product.kt
@@ -2,7 +2,7 @@ package com.mtjin.nomoneytrip.data.home
 
 data class Product(
     var id: String = "",
-    var image: String = "",
+    var image: List<String> = ArrayList(),
     var content: String = "",
     var city: String = "",
     var local: String = "",
