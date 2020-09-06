04ea6b3738d6581ee79755765c6ad1ed25549c1e
jackjackE <tmddjs210@naver.com>
2020-09-07 00:45:05 +0900

    [Implement] LodgmentDetailFragment.kt 값 세팅 구현

--- NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/lodgment_detail/LodgmentDetailFragment.kt
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/lodgment_detail/LodgmentDetailFragment.kt
@@ -1,5 +1,6 @@
 package com.mtjin.nomoneytrip.views.lodgment_detail
 
+import androidx.navigation.fragment.navArgs
 import com.mtjin.nomoneytrip.R
 import com.mtjin.nomoneytrip.base.BaseFragment
 import com.mtjin.nomoneytrip.databinding.FragmentLodgementDetailBinding
@@ -8,7 +9,18 @@ import kotlinx.android.synthetic.main.fragment_lodgement_detail.*
 
 class LodgmentDetailFragment :
     BaseFragment<FragmentLodgementDetailBinding>(R.layout.fragment_lodgement_detail) {
+    private val productArg: LodgmentDetailFragmentArgs by navArgs()
+
     override fun init() {
+        processIntent()
+        initTmap()
+    }
+
+    private fun processIntent() {
+        binding.product = productArg.product
+    }
+
+    private fun initTmap() {
         val tmapview = TMapView(context)
         tmapview.setSKTMapApiKey(getString(R.string.tmap_key))
         val tMapView = TMapView(context)
