403cb43f0c2b41c2665b4e68f3e8a852d16e5758
jackjackE <tmddjs210@naver.com>
2020-09-06 17:31:53 +0900

    [Add] ReservationCompleteFragment.kt 추가

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/reservation_complete/ReservationCompleteFragment.kt
@@ -0,0 +1,12 @@
+package com.mtjin.nomoneytrip.views.reservation_complete
+
+import com.mtjin.nomoneytrip.R
+import com.mtjin.nomoneytrip.base.BaseFragment
+import com.mtjin.nomoneytrip.databinding.FragmentReservationCompleteBinding
+
+class ReservationCompleteFragment :
+    BaseFragment<FragmentReservationCompleteBinding>(R.layout.fragment_reservation_complete) {
+    override fun init() {
+        
+    }
+}
