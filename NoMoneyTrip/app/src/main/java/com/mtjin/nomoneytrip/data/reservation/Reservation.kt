424981dc2e1e8558ffe607f5356139836ace8a97
jackjackE <tmddjs210@naver.com>
2020-09-08 15:33:32 +0900

    [Revise] Reservation.kt state 프로퍼티 추가

--- NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/data/reservation/Reservation.kt
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/data/reservation/Reservation.kt
@@ -10,6 +10,7 @@ data class Reservation(
     var option: String = "",
     var productId: String = "",
     var num: String = "",
+    var state: Boolean = true, //예약취소시 false
     var startDateTimestamp: Long = 0,
     var endDateTimestamp: Long = 0
 ) : Parcelable
