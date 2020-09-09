4be0eeba3cd7b0f9f2629d89074b9bc50baa7c16
jackjackE <tmddjs210@naver.com>
2020-09-10 00:46:15 +0900

    [Revise] CommunityRepository.kt 인터페이스 메소드 정의

--- NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/data/community/source/CommunityRepository.kt
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/data/community/source/CommunityRepository.kt
@@ -1,4 +1,8 @@
 package com.mtjin.nomoneytrip.data.community.source
 
+import com.mtjin.nomoneytrip.data.reservation.Reservation
+import io.reactivex.Single
+
 interface CommunityRepository {
+    fun requestMyReservations(): Single<List<Reservation>>
 }
--- NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/data/community/source/CommunityRepositoryImpl.kt
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/data/community/source/CommunityRepositoryImpl.kt
@@ -1,4 +1,10 @@
 package com.mtjin.nomoneytrip.data.community.source
 
+import com.mtjin.nomoneytrip.data.reservation.Reservation
+import io.reactivex.Single
+
 class CommunityRepositoryImpl : CommunityRepository {
+    override fun requestMyReservations(): Single<List<Reservation>> {
+        TODO("Not yet implemented")
+    }
 }
