9e9b021dec772cf35429b7474300f75e7105a766
jackjackE <tmddjs210@naver.com>
2020-09-10 01:06:24 +0900

    [Revise] CommunityFragment.kt, CommunityViewModel.kt 후기작성 플로팅 버튼 로직 구현

--- NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/community/CommunityFragment.kt
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/community/CommunityFragment.kt
@@ -1,12 +1,31 @@
 package com.mtjin.nomoneytrip.views.community
 
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.mtjin.nomoneytrip.R
 import com.mtjin.nomoneytrip.base.BaseFragment
 import com.mtjin.nomoneytrip.databinding.FragmentCommunityBinding
 
 class CommunityFragment : BaseFragment<FragmentCommunityBinding>(R.layout.fragment_community) {
+    private val viewModel: CommunityViewModel by viewModels()
     override fun init() {
-        binding
+        binding.vm = viewModel
+        initViewModelCallback()
+    }
+
+    private fun initViewModelCallback() {
+        with(viewModel) {
+            goTourHistory.observe(this@CommunityFragment, Observer {
+                findNavController().navigate(
+                    CommunityFragmentDirections.actionBottomNav2ToTourHistoryFragment(it.toTypedArray())
+                )
+            })
+
+            goTourNoHistory.observe(this@CommunityFragment, Observer {
+                findNavController().navigate(CommunityFragmentDirections.actionBottomNav2ToTourNoHistoryFragment())
+            })
+        }
     }
 
 }
--- NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/community/CommunityViewModel.kt
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/community/CommunityViewModel.kt
@@ -1,17 +1,39 @@
 package com.mtjin.nomoneytrip.views.community
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import com.mtjin.nomoneytrip.base.BaseViewModel
 import com.mtjin.nomoneytrip.data.community.source.CommunityRepository
+import com.mtjin.nomoneytrip.data.reservation.Reservation
 import com.mtjin.nomoneytrip.utils.SingleLiveEvent
+import com.mtjin.nomoneytrip.utils.TAG
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
 
 class CommunityViewModel(private val repository: CommunityRepository) : BaseViewModel() {
 
-    private val _goReview = SingleLiveEvent<Unit>()
+    private val _goTourHistory = SingleLiveEvent<List<Reservation>>()
+    private val _goTourNoHistory = SingleLiveEvent<Unit>()
 
-    val goReview: LiveData<Unit> get() = _goReview
+    val goTourHistory: LiveData<List<Reservation>> get() = _goTourHistory
+    val goTourNoHistory: LiveData<Unit> get() = _goTourNoHistory
 
     fun goReview() {
+        compositeDisposable.add(
+            repository.requestMyReservations()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeBy(
+                    onSuccess = {
+                        if (it.isEmpty()) _goTourHistory.value = it
+                        else _goTourNoHistory.call()
 
+                    },
+                    onError = {
+                        Log.d(TAG, "CommunityViewModel goReview() -> $it")
+                    }
+                )
+        )
     }
 }
