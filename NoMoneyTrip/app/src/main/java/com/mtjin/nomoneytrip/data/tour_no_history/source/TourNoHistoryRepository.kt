7b299374e5d2e30bfc05494242c491bcc2df8869
jackjackE <tmddjs210@naver.com>
2020-09-10 20:00:11 +0900

    [Implement] TourNoHistoryRepository.kt 구현

--- NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/data/tour_no_history/source/TourNoHistoryRepository.kt
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/data/tour_no_history/source/TourNoHistoryRepository.kt
@@ -1,4 +1,8 @@
 package com.mtjin.nomoneytrip.data.tour_no_history.source
 
+import com.mtjin.nomoneytrip.data.home.Product
+import io.reactivex.Single
+
 interface TourNoHistoryRepository {
+    fun requestProducts(): Single<List<Product>>
 }
--- NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/data/tour_no_history/source/TourNoHistoryRepositoryImpl.kt
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/data/tour_no_history/source/TourNoHistoryRepositoryImpl.kt
@@ -1,6 +1,38 @@
 package com.mtjin.nomoneytrip.data.tour_no_history.source
 
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.mtjin.nomoneytrip.data.home.Product
+import com.mtjin.nomoneytrip.utils.PRODUCT
+import io.reactivex.Single
+import java.util.*
+import kotlin.collections.ArrayList
 
 class TourNoHistoryRepositoryImpl(private val database: DatabaseReference) :
-    TourNoHistoryRepository
+    TourNoHistoryRepository {
+    override fun requestProducts(): Single<List<Product>> {
+        return Single.create<List<Product>> { emitter ->
+            database.child(PRODUCT).addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onCancelled(error: DatabaseError) {
+                    emitter.onError(error.toException())
+                }
+
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val productList = ArrayList<Product>()
+                    for (productSnapShot: DataSnapshot in snapshot.children) {
+                        productSnapShot.getValue(Product::class.java)?.let {
+                            productList.add(it)
+                        }
+                    }
+                    val seed = System.nanoTime()
+                    productList.shuffle(Random(seed))
+                    emitter.onSuccess(productList)
+                }
+
+            })
+        }
+
+    }
+}
