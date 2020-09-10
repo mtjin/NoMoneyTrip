242b4c2239f07afdd533044c242fd09b30c8efd2
jackjackE <tmddjs210@naver.com>
2020-09-10 21:20:00 +0900

    [Revise] MainActivity.kt 상품 Test Data 추가하는 함수 추가

--- NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/main/MainActivity.kt
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/main/MainActivity.kt
@@ -8,9 +8,13 @@ import android.util.Log
 import android.view.View
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.mtjin.nomoneytrip.R
 import com.mtjin.nomoneytrip.base.BaseActivity
+import com.mtjin.nomoneytrip.data.home.Product
 import com.mtjin.nomoneytrip.databinding.ActivityMainBinding
+import com.mtjin.nomoneytrip.utils.PRODUCT
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
 
@@ -21,14 +25,40 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         super.onCreate(savedInstanceState)
         initNavigation()
         getHashKey()
-//        val tmapview = TMapView(this)
-//        tmapview.setSKTMapApiKey(getString(R.string.tmap_key))
-//
-//        val linearLayoutTmap = findViewById<LinearLayout>(R.id.linearLayoutTmap)
-//        val tMapView = TMapView(this)
-//
-//        tMapView.setSKTMapApiKey(getString(R.string.tmap_key))
-//        linearLayoutTmap.addView(tMapView)
+        insertProductTestData()
+    }
+
+    private fun insertProductTestData() {
+        for (i in 1..5) {
+            Firebase.database.reference.child(PRODUCT).child(i.toString())
+                .setValue(
+                    Product(
+                        address = "충청남도 청양군 대치면 까치내로 1063-1",
+                        animal = true,
+                        checkIn = "오전 9시",
+                        checkOut = "다음날 오전 11시",
+                        city = "서울",
+                        content = "청양 칠갑산 산꽃마을",
+                        hashTagList = listOf("#농촌 봉사", "#농촌 체험"),
+                        homePage = "https://www.naver.com/",
+                        id = i.toString(),
+                        imageList = listOf(
+                            "https://firebasestorage.googleapis.com/v0/b/nomoneytrip-63056.appspot.com/o/1.PNG?alt=media&token=7dc1ae5d-d32a-422b-8ebe-3c4cfcabae9b",
+                            "https://firebasestorage.googleapis.com/v0/b/nomoneytrip-63056.appspot.com/o/2.PNG?alt=media&token=874f89c0-d134-4981-8be4-4f690deaf510"
+                        ),
+                        internet = true,
+                        market = true,
+                        optionList = listOf("농촌 벼농사", "할매 할부지 앞에서 장기자랑"),
+                        parking = true,
+                        phone = "041-944-2007",
+                        time = "일손 4시간",
+                        title = "초록초록 농촌마을에서 봉사를 같이하시죠",
+                        xPos = "36.412677",
+                        yPos = "126.830979"
+                    )
+
+                )
+        }
     }
 
     private fun initNavigation() {
