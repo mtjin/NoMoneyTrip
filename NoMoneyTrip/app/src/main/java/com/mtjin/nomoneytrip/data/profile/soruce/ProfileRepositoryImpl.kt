4dee8247b44f15ee154341f4b65a16631bedb185
jackjackE <tmddjs210@naver.com>
2020-09-11 02:49:21 +0900

    [Add] ProfileViewModel.kt, ProfileRepository.kt 추가

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/data/profile/soruce/ProfileRepositoryImpl.kt
@@ -0,0 +1,5 @@
+package com.mtjin.nomoneytrip.data.profile.soruce
+
+import com.google.firebase.database.DatabaseReference
+
+class ProfileRepositoryImpl(private val database: DatabaseReference) : ProfileRepository
---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/profile/ProfileViewModel.kt
@@ -0,0 +1,6 @@
+package com.mtjin.nomoneytrip.views.profile
+
+import com.mtjin.nomoneytrip.base.BaseViewModel
+import com.mtjin.nomoneytrip.data.profile.soruce.ProfileRepository
+
+class ProfileViewModel(private val profileRepository: ProfileRepository) : BaseViewModel()
