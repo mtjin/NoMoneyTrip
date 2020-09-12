6d7d2e04193127dac4328a913680557a57679f4e
jackjackE <tmddjs210@naver.com>
2020-09-12 21:00:37 +0900

    [Add] WebViewFragment.kt 추가

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/web_view/WebViewFragment.kt
@@ -0,0 +1,12 @@
+package com.mtjin.nomoneytrip.views.web_view
+
+import com.mtjin.nomoneytrip.R
+import com.mtjin.nomoneytrip.base.BaseFragment
+import com.mtjin.nomoneytrip.databinding.FragmentWebViewBinding
+
+class WebViewFragment : BaseFragment<FragmentWebViewBinding>(R.layout.fragment_web_view) {
+    override fun init() {
+        
+    }
+
+}
