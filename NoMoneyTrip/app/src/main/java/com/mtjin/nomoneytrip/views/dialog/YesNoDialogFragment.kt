9bf62d6fc6dae0992298131c48794569e15df65d
jackjackE <tmddjs210@naver.com>
2020-09-21 03:07:19 +0900

    [Add] YesNoDialogFragment.kt 추가

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/dialog/YesNoDialogFragment.kt
@@ -0,0 +1,38 @@
+package com.mtjin.nomoneytrip.views.dialog
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.mtjin.nomoneytrip.R
+import kotlinx.android.synthetic.main.fragment_dialog_yes_no.view.*
+
+class YesNoDialogFragment : DialogFragment(), View.OnClickListener {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_dialog_yes_no, container, false)
+        //어느 다이어로그에서 왔는지
+//        val bundle = arguments
+//        val notice = bundle.getParcelable<FavoriteNotice>(EXTRA_NOTICE_SAVE)
+        view.tv_yes.setOnClickListener {
+
+        }
+        view.tv_no.setOnClickListener {
+
+        }
+
+        return view
+    }
+
+    fun getInstance(): YesNoDialogFragment {
+        return YesNoDialogFragment()
+    }
+
+    override fun onClick(p0: View?) {
+        dismiss()
+    }
+}
