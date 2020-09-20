fb940be8eb1f4ccab4b31e4fb2c82998539a68ba
jackjackE <tmddjs210@naver.com>
2020-09-21 05:53:12 +0900

    [Revise] PhoneAuthFragment.kt -> PhoneAuthActivity.kt 로 변경

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/phone/PhoneAuthActivity.kt
@@ -0,0 +1,8 @@
+package com.mtjin.nomoneytrip.views.phone
+
+import com.mtjin.nomoneytrip.R
+import com.mtjin.nomoneytrip.base.BaseActivity
+import com.mtjin.nomoneytrip.databinding.ActivityPhoneAuthBinding
+
+class PhoneAuthActivity : BaseActivity<ActivityPhoneAuthBinding>(R.layout.activity_phone_auth) {
+}
