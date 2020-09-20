99d5fd4bd1c1494dcdada744f01055d2049b34df
jackjackE <tmddjs210@naver.com>
2020-09-20 21:43:07 +0900

    [Add] SettingFragment.kt 추가

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/setting/SettingFragment.kt
@@ -0,0 +1,11 @@
+package com.mtjin.nomoneytrip.views.setting
+
+import com.mtjin.nomoneytrip.R
+import com.mtjin.nomoneytrip.base.BaseFragment
+import com.mtjin.nomoneytrip.databinding.FragmentSettingBinding
+
+class SettingFragment : BaseFragment<FragmentSettingBinding>(R.layout.fragment_setting) {
+    override fun init() {
+        
+    }
+}
