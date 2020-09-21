10f8cc15795f98e23a12d50cb659e46ebb610401
jackjackE <tmddjs210@naver.com>
2020-09-21 14:16:36 +0900

    [Add] MasterUser.kt 추가

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/data/email_login/MasterUser.kt
@@ -0,0 +1,8 @@
+package com.mtjin.nomoneytrip.data.email_login
+
+data class MasterUser(
+    var id: String = "",
+    var pw: String = "",
+    var tel: String = "",
+    var productId: String = ""
+)
