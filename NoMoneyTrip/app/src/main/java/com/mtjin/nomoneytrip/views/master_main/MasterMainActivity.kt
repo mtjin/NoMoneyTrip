81d277c8f6f1f44d36659c18d5a373e4e97a4e9e
jackjackE <tmddjs210@naver.com>
2020-09-21 14:48:27 +0900

    [Add] MasterMainActivity.kt 추가

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/master_main/MasterMainActivity.kt
@@ -0,0 +1,12 @@
+package com.mtjin.nomoneytrip.views.master_main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mtjin.nomoneytrip.R
+
+class MasterMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_master_main)
+    }
+}
