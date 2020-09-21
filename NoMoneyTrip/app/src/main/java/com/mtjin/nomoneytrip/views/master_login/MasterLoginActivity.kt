7874cd4d692d49b1ee68663ae039cd8f62773964
jackjackE <tmddjs210@naver.com>
2020-09-21 13:51:55 +0900

    [Add] MasterLoginActivity.kt 추가

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/master_login/MasterLoginActivity.kt
@@ -0,0 +1,13 @@
+package com.mtjin.nomoneytrip.views.master_login
+
+import android.os.Bundle
+import com.mtjin.nomoneytrip.R
+import com.mtjin.nomoneytrip.base.BaseActivity
+import com.mtjin.nomoneytrip.databinding.ActivityMasterLoginBinding
+
+class MasterLoginActivity :
+    BaseActivity<ActivityMasterLoginBinding>(R.layout.activity_master_login) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
