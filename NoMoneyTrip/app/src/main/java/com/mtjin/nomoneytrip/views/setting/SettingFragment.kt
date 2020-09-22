a80b31445241d563461dcc4f8ba41eec7bfaf5ec
jackjackE <tmddjs210@naver.com>
2020-09-22 21:30:48 +0900

    [Revise] SettingFragment.kt 개인정보처리방침 url 연결

--- NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/setting/SettingFragment.kt
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/setting/SettingFragment.kt
@@ -2,10 +2,14 @@ package com.mtjin.nomoneytrip.views.setting
 
 import android.content.Intent
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.mtjin.nomoneytrip.R
 import com.mtjin.nomoneytrip.base.BaseFragment
 import com.mtjin.nomoneytrip.databinding.FragmentSettingBinding
+import com.mtjin.nomoneytrip.utils.APP_LOCATION_INFO_RULE
+import com.mtjin.nomoneytrip.utils.APP_PERSONAL_INFO_RULE
+import com.mtjin.nomoneytrip.utils.APP_RULE_URL
 import com.mtjin.nomoneytrip.views.login.LoginActivity
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -32,19 +36,29 @@ class SettingFragment : BaseFragment<FragmentSettingBinding>(R.layout.fragment_s
                 requireActivity().startActivity(Intent(requireContext(), LoginActivity::class.java))
                 requireActivity().finish()
             })
-            onInfoClick.observe(this@SettingFragment, Observer {
-                showToast("준비중")
+            onInfoClick.observe(this@SettingFragment, Observer {//앱정보
+
             })
-            onRuleClick.observe(this@SettingFragment, Observer {
-                showToast("준비중")
+            onRuleClick.observe(this@SettingFragment, Observer { //이용약관
+                findNavController().navigate(
+                    SettingFragmentDirections.actionSettingFragmentToWebViewFragment(APP_RULE_URL)
+                )
             })
-            onPersonalInfoClick.observe(this@SettingFragment, Observer {
-                showToast("준비중")
+            onPersonalInfoClick.observe(this@SettingFragment, Observer { //개인정보처리방침
+                findNavController().navigate(
+                    SettingFragmentDirections.actionSettingFragmentToWebViewFragment(
+                        APP_PERSONAL_INFO_RULE
+                    )
+                )
             })
-            onLocationInfoClick.observe(this@SettingFragment, Observer {
-                showToast("준비중")
+            onLocationInfoClick.observe(this@SettingFragment, Observer { //위치정보이용약관
+                findNavController().navigate(
+                    SettingFragmentDirections.actionSettingFragmentToWebViewFragment(
+                        APP_LOCATION_INFO_RULE
+                    )
+                )
             })
-            onDeleteAuthClick.observe(this@SettingFragment, Observer {
+            onDeleteAuthClick.observe(this@SettingFragment, Observer { //회원탈퇴
                 showToast("준비중")
             })
         }
