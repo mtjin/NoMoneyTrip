eaf7628e00363faea89aba1d2d64c251aec0dd4a
jackjackE <tmddjs210@naver.com>
2020-09-22 21:42:25 +0900

    [Add] AppInfoFragment.kt 추가

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/app_info/AppInfoFragment.kt
@@ -0,0 +1,11 @@
+package com.mtjin.nomoneytrip.views.app_info
+
+import com.mtjin.nomoneytrip.R
+import com.mtjin.nomoneytrip.base.BaseFragment
+import com.mtjin.nomoneytrip.databinding.FragmentAppInfoBinding
+
+class AppInfoFragment : BaseFragment<FragmentAppInfoBinding>(R.layout.fragment_app_info) {
+    override fun init() {
+
+    }
+}
