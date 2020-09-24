64f05b6c6cb3fdc72d539d0f9bc637c1e31080e3
jackjackE <tmddjs210@naver.com>
2020-09-25 02:54:33 +0900

    [Add] FavoriteFragment.kt 저장한 여행 화면 추가

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/favorite/FavoriteFragment.kt
@@ -0,0 +1,12 @@
+package com.mtjin.nomoneytrip.views.favorite
+
+import com.mtjin.nomoneytrip.R
+import com.mtjin.nomoneytrip.base.BaseFragment
+import com.mtjin.nomoneytrip.databinding.FragmentFavoriteBinding
+
+class FavoriteFragment : BaseFragment<FragmentFavoriteBinding>(R.layout.fragment_favorite) {
+    override fun init() {
+        
+    }
+
+}
