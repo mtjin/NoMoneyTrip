d524cf78343874bf5c110ba0580d6baa662027b7
jackjackE <tmddjs210@naver.com>
2020-09-25 19:40:38 +0900

    [Add] RecommendFragment.kt 내가 추천한 게시글 화면 추가

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/recommend/RecommendFragment.kt
@@ -0,0 +1,12 @@
+package com.mtjin.nomoneytrip.views.recommend
+
+import com.mtjin.nomoneytrip.R
+import com.mtjin.nomoneytrip.base.BaseFragment
+import com.mtjin.nomoneytrip.databinding.FragmentNoHistoryRecommendBinding
+
+class RecommendFragment :
+    BaseFragment<FragmentNoHistoryRecommendBinding>(R.layout.fragment_recommend) {
+    override fun init() {
+        
+    }
+}
