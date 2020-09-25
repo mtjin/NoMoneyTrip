4154f22ac37c26a6256d575a856bb5bba2c141c7
jackjackE <tmddjs210@naver.com>
2020-09-25 18:29:19 +0900

    [Implement] FavoriteAdapter.kt 구현

--- NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/favorite/FavoriteAdapter.kt
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/favorite/FavoriteAdapter.kt
@@ -1,4 +1,57 @@
 package com.mtjin.nomoneytrip.views.favorite
 
-class FavoriteAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.mtjin.nomoneytrip.R
+import com.mtjin.nomoneytrip.data.home.Product
+import com.mtjin.nomoneytrip.databinding.ItemFavoriteBinding
+
+class FavoriteAdapter(
+    private val onFavoriteClick: (Product) -> Unit
+) :
+    RecyclerView.Adapter<FavoriteAdapter.ViewHolder>() {
+    private val items = ArrayList<Product>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding: ItemFavoriteBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.item_favorite,
+            parent,
+            false
+        )
+        val viewHolder = ViewHolder(binding)
+        binding.ivSave.setOnClickListener {
+            onFavoriteClick(items[viewHolder.adapterPosition])
+        }
+        return viewHolder
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        items[position].let {
+            holder.bind(it)
+        }
+    }
+
+    inner class ViewHolder(private val binding: ItemFavoriteBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: Product) {
+            binding.item = item
+            binding.executePendingBindings()
+        }
+    }
+
+    fun addItems(items: List<Product>) {
+        this.items.addAll(items)
+        notifyDataSetChanged()
+    }
+
+    fun clear() {
+        this.items.clear()
+        notifyDataSetChanged()
+    }
 }
