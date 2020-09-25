0ad1749bad29af1adb2b12e947cd6b308a6f05d4
jackjackE <tmddjs210@naver.com>
2020-09-25 19:43:53 +0900

    [Rename] RecommendFragment.kt -> RecommendPostFragment.kt 네이밍 변경

--- NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/recommend_post/RecommendPostFragment.kt
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/recommend_post/RecommendPostFragment.kt
@@ -1,12 +1,12 @@
-package com.mtjin.nomoneytrip.views.recommend
+package com.mtjin.nomoneytrip.views.recommend_post
 
 import com.mtjin.nomoneytrip.R
 import com.mtjin.nomoneytrip.base.BaseFragment
 import com.mtjin.nomoneytrip.databinding.FragmentNoHistoryRecommendBinding
 
-class RecommendFragment :
-    BaseFragment<FragmentNoHistoryRecommendBinding>(R.layout.fragment_recommend) {
+class RecommendPostFragment :
+    BaseFragment<FragmentNoHistoryRecommendBinding>(R.layout.fragment_recommend_post) {
     override fun init() {
-        
+
     }
 }
