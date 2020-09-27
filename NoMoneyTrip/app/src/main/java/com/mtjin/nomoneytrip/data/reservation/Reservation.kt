7aec5f90f4f46bcf8793362cb00e02ba8521e8b7
jackjackE <tmddjs210@naver.com>
2020-09-28 04:01:30 +0900

    [Revise] Reservation.kt 데이터 클래스 감사편지 남김 유무 프로퍼티 추가

--- NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/data/reservation/Reservation.kt
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/data/reservation/Reservation.kt
@@ -13,5 +13,6 @@ data class Reservation(
     val reviewed: Boolean = false, // 고객 리뷰 남김 유무
     var startDateTimestamp: Long = 0,
     var endDateTimestamp: Long = 0,
-    var state: Int = 0 // 0:기본값 대기상태(미정), 1: 이장 예약거절, 2:이장 예약수락, 3: 사용자예약취소
+    var state: Int = 0, // 0:기본값 대기상태(미정), 1: 이장 예약거절, 2:이장 예약수락, 3: 사용자예약취소
+    var masterLetter: Boolean = false // 이장님 감사편지 남김 유무
 ) : Parcelable
