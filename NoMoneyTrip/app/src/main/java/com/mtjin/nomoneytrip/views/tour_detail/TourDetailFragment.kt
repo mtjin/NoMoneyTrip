f2b070c08de948a71f6b794059139216fc23bbaf
jackjackE <tmddjs210@naver.com>
2020-09-28 19:56:06 +0900

    [Add] TourDetailFragment.kt, TourDetailViewModel.kt 추가

---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/tour_detail/TourDetailFragment.kt
@@ -0,0 +1,11 @@
+package com.mtjin.nomoneytrip.views.tour_detail
+
+import com.mtjin.nomoneytrip.R
+import com.mtjin.nomoneytrip.base.BaseFragment
+import com.mtjin.nomoneytrip.databinding.FragmentTourDetailBinding
+
+class TourDetailFragment : BaseFragment<FragmentTourDetailBinding>(R.layout.fragment_tour_detail) {
+    override fun init() {
+
+    }
+}
---
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/views/tour_detail/TourDetailViewModel.kt
@@ -0,0 +1,6 @@
+package com.mtjin.nomoneytrip.views.tour_detail
+
+import com.mtjin.nomoneytrip.base.BaseViewModel
+
+class TourDetailViewModel : BaseViewModel() {
+}
