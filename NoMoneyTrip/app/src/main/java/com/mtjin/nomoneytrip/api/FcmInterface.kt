08d26023f7ec7fbdb0ed8ff69a430841b635e79d
jackjackE <tmddjs210@naver.com>
2021-02-08 22:01:08 +0900

    [Revise] NotificationBody.kt 패키지 import 수정

--- NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/api/FcmInterface.kt
+++ NoMoneyTrip/app/src/main/java/com/mtjin/nomoneytrip/api/FcmInterface.kt
@@ -1,6 +1,6 @@
 package com.mtjin.nomoneytrip.api
 
-import com.mtjin.nomoneytrip.service.NotificationBody
+import com.mtjin.nomoneytrip.service.notification.NotificationBody
 import io.reactivex.Single
 import okhttp3.ResponseBody
 import retrofit2.http.Body
